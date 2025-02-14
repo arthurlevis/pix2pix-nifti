7976665a054a1113d41c0f22cdddc222f4b6ca94
ArthurLevi10 <arthurlevi10@Ubuntu20046LTS.myguest.virtualbox.org>
2025-02-14 03:01:47 +0000

    Changed default options

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -28,11 +28,21 @@ class Pix2PixModel(BaseModel):
         The training objective is: GAN Loss + lambda_L1 * ||G(A)-B||_1
         By default, we use vanilla GAN loss, UNet with batchnorm, and aligned datasets.
         """
-        # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
-        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
+        # # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
+        # parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
+        # if is_train:
+        #     parser.set_defaults(pool_size=0, gan_mode='vanilla')
+        #     parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
+
+        # return parser
+
+        # Use default values from options scripts for --norm, --netG, --gan_mode
+        # Keep --dataset_mode as aligned & --pool_size as 0 because suited for pix2pix
+        # Set --lambda_L1 to 50 (Alain-Beaudoin et al., 2023)
+        parser.set_defaults(norm='instance', netG='resnet_9blocks', dataset_mode='aligned')
         if is_train:
-            parser.set_defaults(pool_size=0, gan_mode='vanilla')
-            parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
+            parser.set_defaults(pool_size=0, gan_mode='lsgan')
+            parser.add_argument('--lambda_L1', type=float, default=50.0, help='weight for L1 loss')
 
         return parser
 
